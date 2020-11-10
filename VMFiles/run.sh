476965d90b76cb60cd5fee38c1dfdc043d2b402a
Jake Jepson <55201008+Jepson2k@users.noreply.github.com>
2020-11-09 21:44:48 -0700

    Changed the run script to work with the new docker file

--- VMFiles/run.sh
+++ VMFiles/run.sh
@@ -2,15 +2,16 @@
 
 # This is called a shebang -> hashtag!/bin/sh
 # This is called a bashism -> hashtag!/bin/bash
-
+# cd into correct directory
 cd ~/cs370TermProject1
-git fetch --all
-git reset --hard origin/master
 
-docker rm team22
+#Filter the docker process list and if found then stop the container and remove it
+docker ps -q --filter "name=team22" | grep -q . && docker stop team22 && docker rm -fv team22
+
+#We now need to build the container and tag the build with team 22
 docker build -t team22
 
-# Ways to run it
-docker run --publish 8080:8080 -it --name=team22 team22
-#docker container run team22
-#docker run --publish 8080:8080 team22
+#Run the container with the name team22 and forward incoming traffic from 8080 to the
+# containers port 8080 using the build file team 22
+# -it adds an interactive terminal 
+docker run -it --name=team22 -p 8080:8080 team22
