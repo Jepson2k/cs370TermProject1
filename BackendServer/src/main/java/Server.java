7f5da1807433ed50cab1f6db8cc402fbe215473e
Jake Jepson <55201008+Jepson2k@users.noreply.github.com>
2020-12-10 18:42:12 -0700

    Backend Server Fix

---
+++ BackendServer/src/main/java/Server.java
@@ -0,0 +1,68 @@
+import spark.Spark;
+import spark.Request;
+import spark.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server{
+
+    private final Logger log = LoggerFactory.getLogger(Server.class);
+
+    public Server(){
+        this.configureRestfulApiServer();
+        this.processRestfulApiRequests();
+    }
+
+    private void configureRestfulApiServer(){
+        Spark.port(8080);
+        System.out.println("Server configured to listen on port 8080");
+    }
+
+    private void processRestfulApiRequests(){
+        Spark.get("/", this::echoRequest);
+        Spark.post("/", this::RestfulApiRequestPost);
+    }
+
+    private String RestfulApiRequestPost(Request req, Response res){
+        System.out.println(req.body());
+        return req.body();
+    }
+
+    private String echoRequest(Request request, Response response){
+        response.type("application/json");
+        response.header("Access-Control-Allow-Origin", "*");
+        response.status(200); //OK
+        return HttpRequestToJson(request);
+    }
+
+    private String HttpRequestToJson(Request request){
+        return "{\n"
+                + "\"attributes\":\"" + request.attributes() + "\",\n"
+                + "\"body\":\"" + request.body() + "\",\n"
+                + "\"contentLength\":\"" + request.contentLength() + "\",\n"
+                + "\"contentType\":\"" + request.contentType() + "\",\n"
+                + "\"contextPath\":\"" + request.contextPath() + "\",\n"
+                + "\"cookies\":\"" + request.cookies() + "\",\n"
+                + "\"headers\":\"" + request.headers() + "\",\n"
+                + "\"host\":\"" + request.host() + "\",\n"
+                + "\"ip\":\"" + request.ip() + "\",\n"
+                + "\"params\":\"" + request.params() + "\",\n"
+                + "\"pathInfo\":\"" + request.pathInfo() + "\",\n"
+                + "\"serverPort\":\"" + request.port() + "\",\n"
+                + "\"protocol\":\"" + request.protocol() + "\",\n"
+                + "\"queryParams\":\"" + request.queryParams() + "\",\n"
+                + "\"requestMethod\":\"" + request.requestMethod() + "\",\n"
+                + "\"scheme\":\"" + request.scheme() + "\",\n"
+                + "\"servletPath\":\"" + request.servletPath() + "\",\n"
+                + "\"session\":\"" + request.session() + "\",\n"
+                + "\"uri()\":\"" + request.uri() + "\",\n"
+                + "\"url()\":\"" + request.url() + "\",\n"
+                + "\"userAgent\":\"" + request.userAgent() + "\",\n"
+                + "}";
+    }
+
+    public static void main(String[] programArgs){
+        Server restfulServer = new Server(); //Never returns!
+    }
+}
