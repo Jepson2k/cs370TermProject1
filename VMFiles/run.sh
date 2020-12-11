9cd653a18a82afa7f6a261f509f753acc724ff35
Jake Jepson <55201008+Jepson2k@users.noreply.github.com>
2020-12-11 03:19:57 -0700

    lower case

--- VMFiles/run.sh
+++ VMFiles/run.sh
@@ -14,16 +14,16 @@ docker network create --driver bridge YourMom
 cd ~/cs370TermProject1/BackendServer/
 
 #Filter the docker process list and if found then stop the container and remove it
-docker rm -fv BackendServer
+docker rm -fv backendserver
 
 #We now need to build the container and tag the build with team 22
-docker build -t BackendServer .
+docker build -t backendserver .
 
 #Run the container with the name team22 and forward incoming traffic from 8080 to the
 # containers port 8080 using the build file team 22
 # -it adds an interactive terminal 
 # -d makes it detached
-docker run -d --name=BackendServer --network YourMom -p 8080:8080 BackendServer
+docker run -d --name=backendserver --network YourMom -p 8080:8080 backendserver
 
 # This is called a shebang -> hashtag!/bin/sh
 # This is called a bashism -> hashtag!/bin/bash
@@ -31,13 +31,13 @@ docker run -d --name=BackendServer --network YourMom -p 8080:8080 BackendServer
 cd ~/cs370TermProject1/ClientServer/
 
 #Filter the docker process list and if found then stop the container and remove it
-docker rm -fv ClientServer
+docker rm -fv clientserver
 
 #We now need to build the container and tag the build with team 22
-docker build -t ClientServer .
+docker build -t clientserver .
 
 #Run the container with the name team22 and forward incoming traffic from 8080 to the
 # containers port 8080 using the build file team 22
 # -it adds an interactive terminal 
 # -d makes it detached
-docker run -it --name=ClientServer --network YourMom -p 8080:8080 ClientServer
+docker run -it --name=clientserver --network YourMom -p 8080:8080 clientserver
