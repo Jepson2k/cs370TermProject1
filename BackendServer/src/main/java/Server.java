486a41a8c3d918ef19b3c30ad2251396953db1bf
Jake Jepson <55201008+Jepson2k@users.noreply.github.com>
2020-12-11 00:35:18 -0700

    It really do pprint numbers

--- BackendServer/src/main/java/Server.java
+++ BackendServer/src/main/java/Server.java
@@ -40,7 +40,7 @@ public class Server{
         return HttpRequestToJson(request);
     }
 
-    private String HttpRequestToJson(Request request){
+    private String HttpRequestToJson(Request request) {
         return "{\n"
                 + "\"attributes\":\"" + request.attributes() + "\",\n"
                 + "\"body\":\"" + request.body() + "\",\n"
@@ -62,234 +62,166 @@ public class Server{
                 + "\"session\":\"" + request.session() + "\",\n"
                 + "\"uri()\":\"" + request.uri() + "\",\n"
                 + "\"url()\":\"" + request.url() + "\",\n"
-                + "\"userAgent\":\"" + request.userAgent() + "\",\n"
+                + "\"userAgent\":\"" + request.userAgent() + "\"\n"
                 + "}";
     }
 
     private String passwordStrength(String password){
         int length = password.length();
-        List<Float> alphanumericProbability = characterProbability(password, length);
-        float entropy = 0;
+        float[] arrayOfEntropy = shannonEntropy(password, length);
 
-        // Calculate Shannon entropy
-        for (int i = 0; i < alphanumericProbability.size(); i++) {
-            entropy += alphanumericProbability.get(i) * Math.log(alphanumericProbability.get(i)) / Math.log(2);
-        }
+        System.out.printf("%.2f %.2f %.2f", arrayOfEntropy[0], arrayOfEntropy[1], arrayOfEntropy[2]);
 
-        int entropyRounded = 0;
-        if
-        entroyRounded = Math.round((Math.round(entropy * 4)/4f) * 100);
-        String verbalRating = "Abysmal";
-        switch (entropyRounded) {
-            case 25:
-                verbalRating = "Heinous";
-                break;
-            case 50:
-                verbalRating = "Revolting";
-                break;
-            case 75:
-                verbalRating = "Vile";
-                break;
-            case 100:
-                verbalRating = "Horrendous";
-                break;
-            case 125:
-                verbalRating = "Atrocious";
-                break;
-            case 150:
-                verbalRating = "Appalling";
-                break;
-            case 175:
-                verbalRating = "Abominable";
-                break;
-            case 200:
-                verbalRating = "Deplorable";
-                break;
-            case 225:
-                verbalRating = "Repulsive";
-                break;
-            case 250:
-                verbalRating = "Despicable";
-                break;
-            case 275:
-                verbalRating = "Dreadful";
-                break;
-            case 300:
-                verbalRating = "Horrid";
-                break;
-            case 325:
-                verbalRating = "Horrible";
-                break;
-            case 350:
-                verbalRating = "Awful";
-                break;
-            case 375:
-                verbalRating = "Terrible";
-                break;
-            case 400:
-                verbalRating = "Shameful";
-                break;
-            case 425:
-                verbalRating = "Very Bad";
-                break;
-            case 450:
-                verbalRating = "Really Bad";
-                break;
-            case 475:
-                verbalRating = "Rubbish";
-                break;
-            case 500:
-                verbalRating = "Unsatisfactory";
-                break;
-            case 525:
-                verbalRating = "Bad";
-                break;
-            case 550:
-                verbalRating = "Poor";
-                break;
-            case 575:
-                verbalRating = "Quite Bad";
-                break;
-            case 600:
-                verbalRating = "Pretty Bad";
-                break;
-            case 625:
-                verbalRating = "Somewhat Bad";
-                break;
-            case 650:
-                verbalRating = "Below Average";
-                break;
-            case 675:
-                verbalRating = "Appalling";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-            case 25:
-                verbalRating = "Horrible";
-                break;
-        }
-
-
-
-        return "Shannon Entropy: -entropy;
+        return "Are you winning son?";
     }
 
-    private List<Float> characterProbability(String password, int passwordLength) {
+    private float[] shannonEntropy(String password, int passwordLength) {
         float[] byteOccurance = new float[256];
-        for (int i = 0; i < byteOccurance.length; i++) {
-            byteOccurance[i] = 0;
-        }
         for (int i = 0; i < passwordLength; i++) {
             byteOccurance[password.charAt(i)]++;
         }
 
+        int lowerCaseUsed = 0;
+        int upperCaseUsed = 0;
+        int digitUsed = 0;
+        int symbolUsed = 0;
+        int characterSet = 0;
+
         for (int i = 0; i < byteOccurance.length; i++) {
-
-        }
-
-        var a = 0, u = 0, n = 0, ns = 0, r = 0, sp = 0, s = 0, chars = 0;
-
-        for (var i = 0 ; i < pass.length; i ++)
-        {
-            var c = pass.charAt(i);
-
-            if (a == 0 && 'abcdefghijklmnopqrstuvwxyz'.indexOf(c) >= 0)
-            {
-                chars += 26;
-                a = 1;
-            }
-            if (u == 0 && 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(c) >= 0)
-            {
-                chars += 26;
-                u = 1;
-            }
-            if (n == 0 && '0123456789'.indexOf(c) >= 0)
-            {
-                chars += 10;
-                n = 1;
-            }
-            if (ns == 0 && '!@#$%^&*()'.indexOf(c) >= 0)
-            {
-                chars += 10;
-                ns = 1;
-            }
-            if (r == 0 && "`~-_=+[{]}\\|;:'\",<.>/?".indexOf(c) >= 0)
-            {
-                chars += 22;
-                r = 1;
-            }
-            if (sp == 0 && c == ' ')
-            {
-                chars += 1;
-                sp = 1;
-            }
-            if (s == 0 && (c < ' ' || c > '~'))
-            {
-                chars += 32 + 128;
-                s = 1;
+            if (byteOccurance[i] != 0) {
+                if (lowerCaseUsed == 0 && i >= 97 && i <= 121) {
+                    characterSet += 26;
+                    lowerCaseUsed = 1;
+                }
+                if (upperCaseUsed == 0 && i >= 65 && i <= 90) {
+                    characterSet += 26;
+                    upperCaseUsed = 1;
+                }
+                if (digitUsed == 0 && i >= 48 && i <= 57) {
+                    characterSet += 10;
+                    digitUsed = 1;
+                }
+                if (symbolUsed == 0 && ((i >= 32 && i <= 47) || (i >= 58 && i <= 64) || (i >= 91 && i <= 96) || (i >= 123 && i <= 126))) {
+                    characterSet += 32;
+                    symbolUsed = 1;
+                }
             }
         }
 
-        return chars;
+        float entropyIfPassTrulyRandom = (float) (Math.log(Math.pow(characterSet, passwordLength))/Math.log(2));
 
+        float entropy = 0;
+        for (int i = 0; i < passwordLength; i++) {
+            if ((i > 3 && i < passwordLength - 3) && ((((int) password.charAt(i) <= 96) && ((int) password.charAt(i) >= 32)) || (((int) password.charAt(i) <= 126) && ((int) password.charAt(i) >= 123)))) {
+                if (i <= 7) entropy += characterSet * 8;
+                if (i >= 9 && i <= 20) entropy += characterSet * 7.5;
+                if (i >= 21) entropy += characterSet * 6;
+            } else {
+                if (i == 0) entropy += characterSet * 4;
+                if (i >= 1 && i <= 2) entropy += characterSet * 2;
+                if (i >= 3 && i <= 7) entropy += characterSet * 2;
+                if (i >= 9 && i <= 20) entropy += characterSet * 1.5;
+                if (i >= 21) entropy += characterSet;
+            }
+        }
+
+        float simplifiedShannonEntropy = 0;
         List<Float> byteProbability = new ArrayList<>();
-        for (int i = 0; i < byteOccurance.length; i++) {
-            if (byteOccurance[i] > 0) byteProbability.add(byteOccurance[i] / passwordLength);
+        for (float v : byteOccurance) {
+            if (v > 0) byteProbability.add(v / passwordLength);
         }
-        return byteProbability;
+        for (Float aFloat : byteProbability) {
+            simplifiedShannonEntropy += aFloat * Math.log(aFloat) / Math.log(2);
+        }
 
-
-
+        return new float[]{-simplifiedShannonEntropy, entropyIfPassTrulyRandom, entropy};
     }
+
+//    private void verbalRating(float[] arrayOfEntropy) {
+//        int entropyRounded = 0;
+//        entroyRounded = Math.round((Math.round(entropy * 4)/4f) * 100);
+//        String verbalRating = "Abysmal";
+//        switch (entropyRounded) {
+//            case 25:
+//                verbalRating = "Heinous";
+//                break;
+//            case 50:
+//                verbalRating = "Revolting";
+//                break;
+//            case 75:
+//                verbalRating = "Vile";
+//                break;
+//            case 100:
+//                verbalRating = "Horrendous";
+//                break;
+//            case 125:
+//                verbalRating = "Atrocious";
+//                break;
+//            case 150:
+//                verbalRating = "Appalling";
+//                break;
+//            case 175:
+//                verbalRating = "Abominable";
+//                break;
+//            case 200:
+//                verbalRating = "Deplorable";
+//                break;
+//            case 225:
+//                verbalRating = "Repulsive";
+//                break;
+//            case 250:
+//                verbalRating = "Despicable";
+//                break;
+//            case 275:
+//                verbalRating = "Dreadful";
+//                break;
+//            case 300:
+//                verbalRating = "Horrid";
+//                break;
+//            case 325:
+//                verbalRating = "Horrible";
+//                break;
+//            case 350:
+//                verbalRating = "Awful";
+//                break;
+//            case 375:
+//                verbalRating = "Terrible";
+//                break;
+//            case 400:
+//                verbalRating = "Shameful";
+//                break;
+//            case 425:
+//                verbalRating = "Very Bad";
+//                break;
+//            case 450:
+//                verbalRating = "Really Bad";
+//                break;
+//            case 475:
+//                verbalRating = "Rubbish";
+//                break;
+//            case 500:
+//                verbalRating = "Unsatisfactory";
+//                break;
+//            case 525:
+//                verbalRating = "Bad";
+//                break;
+//            case 550:
+//                verbalRating = "Poor";
+//                break;
+//            case 575:
+//                verbalRating = "Quite Bad";
+//                break;
+//            case 600:
+//                verbalRating = "Pretty Bad";
+//                break;
+//            case 625:
+//                verbalRating = "Somewhat Bad";
+//                break;
+//            case 650:
+//                verbalRating = "Below Average";
+//                break;
+//        }
+//    }
 
     public static void main(String[] programArgs){
         Server restfulServer = new Server(); //Never returns!
