cec75eb0b2c94e81e2858f49efe6d6445417116c
Jake Jepson <55201008+Jepson2k@users.noreply.github.com>
2020-12-10 22:12:21 -0700

    Backend blazing!

--- BackendServer/src/main/java/Server.java
+++ BackendServer/src/main/java/Server.java
@@ -5,6 +5,9 @@ import spark.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server{
 
     private final Logger log = LoggerFactory.getLogger(Server.class);
@@ -33,6 +36,7 @@ public class Server{
         response.type("application/json");
         response.header("Access-Control-Allow-Origin", "*");
         response.status(200); //OK
+        response.body(passwordStrength(request.body()));
         return HttpRequestToJson(request);
     }
 
@@ -60,6 +64,231 @@ public class Server{
                 + "\"url()\":\"" + request.url() + "\",\n"
                 + "\"userAgent\":\"" + request.userAgent() + "\",\n"
                 + "}";
+    }
+
+    private String passwordStrength(String password){
+        int length = password.length();
+        List<Float> alphanumericProbability = characterProbability(password, length);
+        float entropy = 0;
+
+        // Calculate Shannon entropy
+        for (int i = 0; i < alphanumericProbability.size(); i++) {
+            entropy += alphanumericProbability.get(i) * Math.log(alphanumericProbability.get(i)) / Math.log(2);
+        }
+
+        int entropyRounded = 0;
+        if
+        entroyRounded = Math.round((Math.round(entropy * 4)/4f) * 100);
+        String verbalRating = "Abysmal";
+        switch (entropyRounded) {
+            case 25:
+                verbalRating = "Heinous";
+                break;
+            case 50:
+                verbalRating = "Revolting";
+                break;
+            case 75:
+                verbalRating = "Vile";
+                break;
+            case 100:
+                verbalRating = "Horrendous";
+                break;
+            case 125:
+                verbalRating = "Atrocious";
+                break;
+            case 150:
+                verbalRating = "Appalling";
+                break;
+            case 175:
+                verbalRating = "Abominable";
+                break;
+            case 200:
+                verbalRating = "Deplorable";
+                break;
+            case 225:
+                verbalRating = "Repulsive";
+                break;
+            case 250:
+                verbalRating = "Despicable";
+                break;
+            case 275:
+                verbalRating = "Dreadful";
+                break;
+            case 300:
+                verbalRating = "Horrid";
+                break;
+            case 325:
+                verbalRating = "Horrible";
+                break;
+            case 350:
+                verbalRating = "Awful";
+                break;
+            case 375:
+                verbalRating = "Terrible";
+                break;
+            case 400:
+                verbalRating = "Shameful";
+                break;
+            case 425:
+                verbalRating = "Very Bad";
+                break;
+            case 450:
+                verbalRating = "Really Bad";
+                break;
+            case 475:
+                verbalRating = "Rubbish";
+                break;
+            case 500:
+                verbalRating = "Unsatisfactory";
+                break;
+            case 525:
+                verbalRating = "Bad";
+                break;
+            case 550:
+                verbalRating = "Poor";
+                break;
+            case 575:
+                verbalRating = "Quite Bad";
+                break;
+            case 600:
+                verbalRating = "Pretty Bad";
+                break;
+            case 625:
+                verbalRating = "Somewhat Bad";
+                break;
+            case 650:
+                verbalRating = "Below Average";
+                break;
+            case 675:
+                verbalRating = "Appalling";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+            case 25:
+                verbalRating = "Horrible";
+                break;
+        }
+
+
+
+        return "Shannon Entropy: -entropy;
+    }
+
+    private List<Float> characterProbability(String password, int passwordLength) {
+        float[] byteOccurance = new float[256];
+        for (int i = 0; i < byteOccurance.length; i++) {
+            byteOccurance[i] = 0;
+        }
+        for (int i = 0; i < passwordLength; i++) {
+            byteOccurance[password.charAt(i)]++;
+        }
+
+        for (int i = 0; i < byteOccurance.length; i++) {
+
+        }
+
+        var a = 0, u = 0, n = 0, ns = 0, r = 0, sp = 0, s = 0, chars = 0;
+
+        for (var i = 0 ; i < pass.length; i ++)
+        {
+            var c = pass.charAt(i);
+
+            if (a == 0 && 'abcdefghijklmnopqrstuvwxyz'.indexOf(c) >= 0)
+            {
+                chars += 26;
+                a = 1;
+            }
+            if (u == 0 && 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(c) >= 0)
+            {
+                chars += 26;
+                u = 1;
+            }
+            if (n == 0 && '0123456789'.indexOf(c) >= 0)
+            {
+                chars += 10;
+                n = 1;
+            }
+            if (ns == 0 && '!@#$%^&*()'.indexOf(c) >= 0)
+            {
+                chars += 10;
+                ns = 1;
+            }
+            if (r == 0 && "`~-_=+[{]}\\|;:'\",<.>/?".indexOf(c) >= 0)
+            {
+                chars += 22;
+                r = 1;
+            }
+            if (sp == 0 && c == ' ')
+            {
+                chars += 1;
+                sp = 1;
+            }
+            if (s == 0 && (c < ' ' || c > '~'))
+            {
+                chars += 32 + 128;
+                s = 1;
+            }
+        }
+
+        return chars;
+
+        List<Float> byteProbability = new ArrayList<>();
+        for (int i = 0; i < byteOccurance.length; i++) {
+            if (byteOccurance[i] > 0) byteProbability.add(byteOccurance[i] / passwordLength);
+        }
+        return byteProbability;
+
+
+
     }
 
     public static void main(String[] programArgs){
